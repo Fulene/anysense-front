7adfef81a98f0c33b9aa629c9063f6f0e3411f27
mehdi <mehdi-h@outlook.fr>
2023-08-25 13:22:08 +0200

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  appUri: 'https://anysense.fr',
-  apiHost: 'https://api.anysense.fr/api/',
-  kcHost: 'https://auth.anysense.fr',
+  appUri: 'https://admin-anysense.fr',
+  apiHost: 'https://api.admin-anysense.fr/api/',
+  kcHost: 'https://auth.admin-anysense.fr',
   kcRealm: 'anysense-realm',
   kcClient: 'anysense-front'
 }
