14daa1a80dad9f803b66b7eff94c1514a6c0983f
mehdi <mehdi-h@outlook.fr>
2023-08-27 22:50:41 +0200

    navbar wip

--- src/app/contractor/services/contractor.service.ts
+++ src/app/contractor/services/contractor.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environment } from "../../../environments/environment";
-import { map, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { Contractor } from "../models/contractor";
 
 @Injectable({
