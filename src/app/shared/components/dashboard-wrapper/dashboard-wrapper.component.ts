e70419bc6b61190ac5340a09d50859b494051c88
mehdi <mehdi-h@outlook.fr>
2023-09-07 01:20:47 +0200

    Signup workflow wip

---
+++ src/app/shared/components/dashboard-wrapper/dashboard-wrapper.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-dashboard-wrapper',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard-wrapper.component.html',
+  styleUrls: ['./dashboard-wrapper.component.scss']
+})
+export class DashboardWrapperComponent {
+
+
+
+}
