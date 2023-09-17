7b65b4fcb16c69a74e72f10a24a0a395e9e617c6
mehdi <mehdi-h@outlook.fr>
2023-09-17 03:05:47 +0200

    dashboard responsive style wip...

--- src/app/customer/components/customer-dashboard/customer-dashboard.component.ts
+++ src/app/customer/components/customer-dashboard/customer-dashboard.component.ts
@@ -1,21 +1,24 @@
-import { ChangeDetectorRef, Component, OnDestroy } from '@angular/core';
+import { ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
-import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSidenav, MatSidenavModule } from "@angular/material/sidenav";
 import { MediaMatcher } from "@angular/cdk/layout";
 import { MatListModule } from "@angular/material/list";
 import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-customer-dashboard',
-  standalone: true, imports: [CommonModule, NgOptimizedImage, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterLink, RouterOutlet, MatToolbarModule, RouterLinkActive],
+  standalone: true,
+  imports: [CommonModule, NgOptimizedImage, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule, RouterLink, RouterOutlet, MatToolbarModule, RouterLinkActive],
   templateUrl: './customer-dashboard.component.html',
   styleUrls: ['./customer-dashboard.component.scss']
 })
-export class CustomerDashboardComponent implements OnDestroy {
+export class CustomerDashboardComponent implements OnInit, OnDestroy {
   mobileQuery: MediaQueryList;
+  closedSidenavMode = false;
+  @ViewChild('snav') sidenav?: MatSidenav;
 
   constructor(private router: Router, cdr: ChangeDetectorRef, media: MediaMatcher) {
     this.mobileQuery = media.matchMedia('(max-width: 768px)');
@@ -23,15 +26,35 @@ export class CustomerDashboardComponent implements OnDestroy {
     this.mobileQuery.addEventListener('change', this._mobileQueryListener);
   }
 
-  private readonly _mobileQueryListener: () => void;
-
-  navigateTo(path: string) {
-    // this.router.navigate([path]);
+  ngOnInit(): void {
+    this.navigateTo("/dashboard/test1");
+    if (this.mobileQuery.matches) {
+      this.closedSidenavMode = true;
+    }
   }
 
-    ngOnDestroy() {
-        this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  private readonly _mobileQueryListener: () => void;
+
+  toggleSidenavMode(): void {
+    this.closedSidenavMode = !this.closedSidenavMode;
+    if (this.closedSidenavMode) {
+      this.sidenav!.close();
+    } else {
+      this.sidenav!.open();
     }
+  }
+
+  onSnavClosed() {
+    this.closedSidenavMode = true
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
+  }
+
+  ngOnDestroy() {
+    this.mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
 
 }
 
