8ad4e1c978cf6b94b6204039ca90e07f52823066
mehdi <mehdi-h@outlook.fr>
2023-09-25 19:49:14 +0200

    Added of staging env

---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  appUri: 'https://staging.anysense.fr',
+  apiHost: 'https://api-staging.anysense.fr/api/',
+  kcHost: 'https://auth.anysense.fr',
+  kcRealm: 'anysense-staging-realm',
+  kcClient: 'anysense-front',
+  kcPostLoginRedirectUri: 'https://staging.anysense.fr/dashboard'
+}
