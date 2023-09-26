f122cad6c6fd2b94677f886d6188a0a462c13866
mehdi <mehdi-h@outlook.fr>
2023-09-26 20:21:43 +0200

    return to anysense.fr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  appUri: 'https://admin-anysense.fr',
-  apiHost: 'https://api.admin-anysense.fr/api/',
-  kcHost: 'https://auth.admin-anysense.fr',
+  appUri: 'https://anysense.fr',
+  apiHost: 'https://api.anysense.fr/api/',
+  kcHost: 'https://auth.anysense.fr',
   kcRealm: 'anysense-realm',
   kcClient: 'anysense-front'
 }
