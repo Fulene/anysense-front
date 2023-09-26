f62ec535673a5447c507eefa312ac89e854e1c0d
mehdi <mehdi-h@outlook.fr>
2023-09-27 00:09:11 +0200

    test

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgOptimizedImage } from "@angular/common";
+import { environment } from "../../../../environments/environment";
 
 @Component({
     selector: 'app-home',
@@ -11,4 +12,5 @@ import { NgOptimizedImage } from "@angular/common";
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
+  protected readonly environment = environment;
 }
